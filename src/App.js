2134a10f7a3e363b8e7198e85d10dd63008f0ed8
aleksandre chilingarashvili <alexandre222777@gmail.com>
2024-01-22 13:46:22 +0400

    this is first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import PersonList from "./components/PersonList.js";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <PersonList />
+    </div>
+  );
+}
+
+export default App;
